e75b0dcd8782296668b3f82ca3cd284256233933
jasenmichael <jasen@ready-builder.com>
2018-05-03 00:45:07 -0600

    remove box shadow

--- front-end/src/components/Home.vue
+++ front-end/src/components/Home.vue
@@ -35,7 +35,7 @@
 
 .add:hover {
   transform: scale(1.1);
-  box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+  /* box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22); */
 }
 
 </style>
