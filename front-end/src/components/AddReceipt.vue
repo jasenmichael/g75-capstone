05dadb0b8d05bb989f07def65adbcf87e19af33b
jasenmichael <jasen@ready-builder.com>
2018-05-03 00:44:39 -0600

    implement image input and cropping

--- front-end/src/components/AddReceipt.vue
+++ front-end/src/components/AddReceipt.vue
@@ -4,34 +4,58 @@
       <v-layout column align-center class="">
         <h1 class="display-1">Add an Expense Receipt</h1>
         <div class="butts">
-          <v-flex>
-            <v-btn large  color="secondary" class="mb-5 add" dark>Take a picture it lasts longer..</v-btn>
-          </v-flex>
 
-          <v-flex>
-            <input button id="fileUpload" type="file">
-              <v-btn large  color="secondary" @click.native="openDialog" type="file" class="mb-5 add" dark>Upload File(s) that works too..</v-btn>
-            </input>
-          </v-flex>
+          <div v-show="selectedImage">
+          <div class="ml-4 mb-2" id="imageCrop">
+            <croppa v-model="myCroppa"
+              :width="400"
+              :height="500"
+              :canvas-color="'default'"
+              :placeholder="'Choose an image'"
+              :placeholder-font-size="0"
+              :placeholder-color="'default'"
+              :accept="'image/*'"
+              :file-size-limit="0"
+              :quality="2"
+              :zoom-speed="3"
+              :remove-button-color="'black'"
+              :remove-button-size="0"
+              @image-remove="removeImage"
+              >
+              <img
+                crossOrigin="anonymous"
+                :src="selectedImage"
+                slot="initial">
+            </croppa>
+          </div>
+            <br>
+            <v-btn @click.native="rotateRight"><v-icon>rotate_right</v-icon></v-btn>
+            <v-btn @click.native="rotateLeft"><v-icon>rotate_left</v-icon></v-btn>
+            <!--
+            :initial-image="someImage"
+            @init="handleCroppaInit"
+            @file-choose="handleCroppaFileChoose"
+            @file-size-exceed="handleCroppaFileSizeExceed"
+            @file-type-mismatch="handleCroppaFileTypeMismatch"
+            @move="handleCroppaMove"
+            @zoom="handleCroppaZoom"
+            -->
+          </div>
 
-          <v-flex>
-            <croppa v-model="croppa"
-                     initial-position="center"
-                     :initial-image="initialImage"
-                     :width="350"
-                     :height="630"
-                     placeholder="click to upload files"
-                     :placeholder-font-size="16"
-                     canvas-color="transparent"
-                     :show-remove-button="true"
-                     remove-button-color="black"
-                     :remove-button-size="3"
-                     :show-loading="true">
-             </croppa>
-           </v-flex>
+          <div v-show="!selectedImage">
+            <v-flex>
+              <v-btn large  color="secondary" @click.native="takePicture()" class="mb-5 add" dark>Take a Picture it lasts longer..</v-btn>
+            </v-flex>
+
+            <v-flex>
+                <input button ref="fileUpload" type="file" accept="image/*" hidden @change="selectImageFile"/>
+                <v-btn large  color="secondary" @click.native="$refs.fileUpload.click()" type="file" class="mb-5 add" dark>Or Upload File that works too..</v-btn>
+            </v-flex>
+          </div>
+
+
 
         </div>
-
 
       </v-layout>
     </v-slide-y-transition>
@@ -42,23 +66,46 @@
 export default {
   data () {
     return {
-      myCroppa: {}
+      myCroppa: {},
+      selectedImage: null
     }
   },
   methods: {
-    openDialog () {
-      console.log('hello')
-      // this.$el.fileUpload.click()
+    takePicture () {
+      console.log('took pic')
+      // this.selectedImage = URL.createObjectURL(event.target.files[0])
+      this.selectedImage = null
+      this.myCroppa.refresh()
+    },
+    selectImageFile (event) {
+      console.log('selected an image')
+      this.selectedImage = URL.createObjectURL(event.target.files[0])
+      this.myCroppa.refresh()
+    },
+    rotateRight () {
+      console.log('rotate right')
+      this.myCroppa.rotate()
+    },
+    rotateLeft () {
+      console.log('rotate left')
+      this.myCroppa.rotate(-1)
+    },
+    removeImage () {
+      console.log('image removed')
+      this.selectedImage = null
+      this.myCroppa.refresh()
     },
     uploadCroppedImage () {
-      this.myCroppa.generateBlob((blob) => {
+      this.myCroppa.generateBlob(blob => {
         // write code to upload the cropped image file (a file is a blob)
-      }, 'image/jpeg', 0.8) // 80% compressed jpeg file
+        console.log(blob)
+        this.selectedImage = blob
+      }, 'image/jpeg', 0.8)
     }
   }
 }
 </script>
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 * {
   font-weight: 600;
@@ -85,16 +132,7 @@ h1 {
   box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
 }
 
-
-.croppa-container {
-   background-color: lightblue;
-   border: 2px solid grey;
-   border-radius: 8px;
- }
-
- .croppa-container:hover {
-   opacity: 1;
-   background-color: #8ac9ef;
- }
-
+#imageCrop {
+  margin-top: -4vh;
+}
 </style>
