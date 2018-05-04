6354f66d0b16eb20b2dbc95e15f3f6f711244429
jasenmichael <jasen@ready-builder.com>
2018-05-03 23:28:31 -0600

    create queue component and route

---
+++ front-end/src/components/Queue.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container fluid>
+    <v-layout row wrap>
+      <!-- <h1>{{ $store.state.simpleExpenses.receiptQueue}}</h1> -->
+      <!-- <p>i work</p> -->
+      <v-flex xs6>
+        <div v-for="receipt in $store.state.simpleExpenses.receiptQueue">
+          <!-- <img src="URL.createObjectURL(receipt.dataUrl)"> -->
+          <img :src="receipt.dataUrl">
+        </div>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  // import axios from 'axios'
+  export default {
+    data () {
+      return {}
+    },
+    methods: {
+      receiptObject (blob) {
+        URL.createObjectURL(blob)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+img {
+  width: 320px;
+  height: 400px;
+}
+
+</style>
