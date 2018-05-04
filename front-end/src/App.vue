ff48927ce647f21eccfeb8f4df263cb6adc0bf15
jasenmichael <jasen@ready-builder.com>
2018-05-03 23:29:21 -0600

    add navigation menu items

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -8,21 +8,40 @@
       enable-resize-watcher
       fixed
       app
-      dark
+      light
     >
-      <v-list light>
-        <v-list-tile
-          value="false"
-          v-for="(item, i) in items"
-          :key="i"
-        >
+        <v-list class="pa-5">
+          <v-list-tile avatar>
+            <v-list-tile-avatar size="48px" tile>
+              <img src="@/assets/receipt-icon.png" alt="avatar">
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+                <v-list-tile-title v-text="'Welcome' + (loggedIn ? ' ' + user : '' )"></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+        <hr>
 
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title  + (loggedIn ? ' ' + user : '' )"></v-list-tile-title>
-          </v-list-tile-content>
+        <v-list class="pt-4" dense>
+          <h4 class="ml-4 mb-3">manage your expense reports here</h4>
+          <v-list-tile
+            value="false"
+            v-for="(item, i) in items"
+            :key="i"
+          >
+
+
+            <v-list-tile-content class="m-5">
+               <router-link :to="item.link">
+                <v-btn large class="m-5 links pr-5 links" :href="item.link" router>
+                  <v-icon large class="mr-4" v-html="item.icon"></v-icon>
+                  <p class="mt-3">{{item.title}}<br><span class="small">{{item.desc}}</span></p>
+                </v-btn>
+               </router-link>
+            </v-list-tile-content>
+
+
+
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
@@ -30,17 +49,14 @@
     <v-toolbar
       app
       :clipped-left="clipped"
+      class="primary"
     >
 
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
 
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-
       <div class="mx-auto">
         <router-link to="/">
-          <v-toolbar-title v-text="title"></v-toolbar-title>
+          <v-toolbar-title class="black--text display-2" v-text="title"></v-toolbar-title>
         </router-link>
       </div>
       <v-spacer></v-spacer>
@@ -68,8 +84,34 @@ export default {
       drawer: false,
       fixed: false,
       items: [{
-        icon: 'bubble_chart',
-        title: 'Welcome'
+        icon: 'add',
+        title: 'Add',
+        desc: 'add an expense receipt',
+        link: 'add'
+      },
+      {
+        icon: 'view_list',
+        title: 'List',
+        desc: 'view and edit your records',
+        link: 'list'
+      },
+      {
+        icon: 'library_books',
+        title: 'Queue',
+        desc: 'edit and approve new receipts',
+        link: 'queue'
+      },
+      {
+        icon: 'view_headline',
+        title: 'Reports',
+        desc: 'view and export your records',
+        link: 'reports'
+      },
+      {
+        icon: 'settings',
+        title: 'Settings',
+        desc: 'your settings and categories',
+        link: 'settings'
       }],
       miniVariant: false,
       right: true,
@@ -77,6 +119,29 @@ export default {
       title: '$impleExpenses'
     }
   },
+  mounted () {
+    this.$store.state.loadLocalData()
+  },
   name: 'App'
 }
 </script>
+
+<style>
+
+html {
+  background-color: #7C9886;
+}
+
+v-toolbar {
+  z-index: 3000;
+}
+
+.small {
+  font-size: 60%;
+}
+
+hr {
+  color: black;
+}
+
+</style>
