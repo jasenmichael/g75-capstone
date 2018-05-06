02b781d307e29cd8fc310f8f745ad3258742f2da
jasenmichael <jasen@ready-builder.com>
2018-05-05 18:51:27 -0600

    add component, remove comments

--- front-end/src/store/store.js
+++ front-end/src/store/store.js
@@ -1,23 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 const state = {
-  hello: 'hello from state',
-  loadLocalData () {
-    this.simpleExpenses = JSON.parse(localStorage.getItem('simple_expenses')) || this.simpleExpenses
-  },
   simpleExpenses: {
     id: '',
     user_name: '',
-    categories: [],
-    businesses: [],
+    categories: [1, 2, 3],
+    businesses: ['Deli Cafe', 'Ready Builder', 'Bay Area Ren Faire', 'Michigan Ren Faire'],
     receipts: [],
-    receiptQueue: []
+    receiptQueue: [],
+    edit: null
+  },
+  loadLocalData () {
+    this.simpleExpenses = JSON.parse(localStorage.getItem('simple_expenses')) || this.simpleExpenses
+  },
+  saveLocalData () {
+    localStorage.setItem('simple_expenses', JSON.stringify(this.simpleExpenses))
+    var local = localStorage.getItem('simple_expenses')
+    var store = JSON.stringify(this.simpleExpenses)
+    // console.log(local)
+    // console.log(store)
+    if (local === store) {
+      console.log('you are in sync')
+      console.log('=======')
+    }
   }
 }
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   state
 })
