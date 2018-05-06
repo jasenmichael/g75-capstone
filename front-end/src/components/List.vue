9a294c302af9627643efffc8924da495e0dad5d1
jasenmichael <jasen@ready-builder.com>
2018-05-05 18:52:00 -0600

    create list component

---
+++ front-end/src/components/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="">
+    <EditForm></EditForm>
+
+  </div>
+</template>
+
+<script>
+// build nice layout view with edit button
+// edit button will remove from list and add to queue, and the
+
+  import EditForm from '@/components/EditForm'
+  export default {
+    components: {
+      EditForm
+    },
+    data () {
+      return {
+        valid: false,
+        select: []
+      }
+    }
+  }
+</script>
+
+<style scoped></style>
