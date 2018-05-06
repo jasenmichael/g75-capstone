0da3009682ea25c108eec1dabaa90a52448c8725
jasenmichael <jasen@ready-builder.com>
2018-05-05 18:50:12 -0600

    move form to edit form component

--- front-end/src/components/Queue.vue
+++ front-end/src/components/Queue.vue
@@ -1,113 +1,19 @@
 <template>
-  <v-container fluid>
-    <v-layout row wrap v-for="(receipt, index) in $store.state.simpleExpenses.receiptQueue" :key="index" class="daForm">
-      <v-flex d-flex xs12 sm12 md6 row justify-center>
-          <img :src="receipt.dataUrl" fluid>
-      </v-flex>
-      <v-flex d-flex xs12 sm12 md6 row justify-center>
-          <v-form  v-model="valid" ref="form" lazy-validation>
-
-           <v-layout>
-            <v-flex xs6 class="mr-3">
-               <v-select
-                 label="Business"
-                 v-model="select"
-                 :items="$store.state.simpleExpenses.businesses"
-                 :rules="[v => !!v || 'Item is required']"
-                 required
-               ></v-select>
-            </v-flex>
-            <v-flex xs6>
-               <v-select
-                 label="Category"
-                 v-model="select"
-                 :items="$store.state.simpleExpenses.categories"
-                 :rules="[v => !!v || 'Item is required']"
-                 required
-               ></v-select>
-            </v-flex>
-          </v-layout>
-
-             <v-text-field
-               label="Receipt Issuer"
-               value=""
-               hint='For example, "Home Depot" or "Panera Bread"'
-               persistent-hint
-             ></v-text-field>
-             <v-text-field
-               label="Description"
-               value=""
-               hint='For example, "lunch with a client"'
-               persistent-hint
-             ></v-text-field>
-
-             <!-- money -->
-             <v-layout>
-              <v-flex xs7 class="mr-3">
-                <v-text-field
-                  v-model="receipt.amount"
-                  type="number"
-                  label="Amount"
-                  >
-                </v-text-field>
-              </v-flex>
-              <v-flex xs5>
-                <v-text-field
-                  v-model="receipt.tax"
-                  type="number"
-                  label="Tax"
-                ></v-text-field>
-              </v-flex>
-            </v-layout>
-             <v-btn small round color="info" @click="submit" :disabled="!valid">Submit</v-btn>
-             <v-btn small round color="info" @click="remove">Delete</v-btn>
-           </v-form>
-
-      </v-flex>
-    </v-layout>
-  </v-container>
+      <EditForm></EditForm>
 </template>
 
 <script>
-  import 'vue-croppa/dist/vue-croppa.css'
-  // import axios from 'axios'
+  import EditForm from '@/components/EditForm'
   export default {
+    components: {
+      EditForm
+    },
     data () {
       return {
         valid: false,
         select: []
       }
-    },
-    methods: {
-      receiptObject (blob) {
-        URL.createObjectURL(blob)
-      },
-      reset () {
-        // will refetch this without saving
-      },
-      submit () {},
-      checkbox () {},
-      remove () {}
     }
   }
 </script>
-
-<style scoped>
-
-img {
-  max-width: 320px;
-  max-height: 400px;
-  margin-right: 1rem;
-}
-
-.daForm {
-  margin-bottom: 2rem;
-  padding: 1rem;
-  border: .2rem dotted
-}
-
-.amountSubHead {
-  margin-top: -200px;
-}
-
-</style>
+<style scoped></style>
